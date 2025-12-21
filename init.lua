db82071159ae1e8d1f466ba7295bbbc114258337
m02uku <m02ukuzrzr@gmail.com>
2025-12-21 10:22:03 +0900

    update: cursorcolumn also has to be highlighted

--- init.lua
+++ init.lua
@@ -155,8 +155,9 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
--- Show which line your cursor is on
+-- Show which line/column your cursor is on
 vim.o.cursorline = true
+vim.o.cursorcolumn = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
